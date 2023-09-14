e2973c45cdc389eb903995d8b834a85cc5d5a290
Derek <53124417+DerekLW6@users.noreply.github.com>
2023-09-14 01:03:43 +0000

    Added tested a little bit of code to test the copilot

---
+++ notebooks/01-copilot-test.py
@@ -0,0 +1,15 @@
+# Give me a print statement
+print('Program Starting')
+
+# Import machine learning libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+
+# Import data
+# df = pd.read_csv('data/iris.csv')
+
+
+print('Program Ending')
